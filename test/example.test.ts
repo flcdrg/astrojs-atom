28151a6625ce65c4769f2b7270543a538129a3f1
David Gardiner <david@gardiner.net.au>
2025-04-18 19:48:50 +0930

    almost

--- test/example.test.ts
+++ test/example.test.ts
@@ -1,7 +1,21 @@
 import { AtomFeedItem, atomSchema } from "../src/index";
+import { } from 'astro/zod'
 
 describe("example test", () => {
     it("should be true", () => {
         expect(true).toBe(true);
     });
+
+    it("should validate AtomFeedItem", () => {
+        const item: AtomFeedItem = {
+            title: "Example Title",
+            description: "Example Description",
+            updated: new Date().toISOString(),
+            categories: ["example", "test"]
+        };
+        const result = atomSchema.safeParse(item);
+
+        expect(result.success).toBe(true);
+    });
+
 });
