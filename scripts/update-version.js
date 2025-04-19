b48cf62369d503efe9227f983f3d924fb528c5c1
David Gardiner <david@gardiner.net.au>
2025-04-19 19:02:58 +0930

    Use nbgv to set package version

--- scripts/update-version.js
+++ scripts/update-version.js
@@ -1,7 +1,11 @@
 // This script updates package.json version using nerdbank-gitversioning
-const fs = require('fs');
-const path = require('path');
-const nbgv = require('nerdbank-gitversioning');
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import nbgv from 'nerdbank-gitversioning';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 async function main() {
   const versionInfo = await nbgv.getVersion();
